e421842cbd7c9d9839b914a76ae385a0bdb63f43
Kyle Blair <kblair21@mail.wou.edu>
2026-04-23 16:09:23 -0700

    Changed styling for the card buttons to have them align when the headers differ

--- obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eeef3afc7c5bd48af212dbede2b8ec4efb9e30b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a88ebefbac5615fc142f42ee298d031803ad9539")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
